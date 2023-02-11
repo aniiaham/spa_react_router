0787ad781f34a717eddbe45daa832d4eb0d1ce20
aniiaham <aniya_axm@icloud.com>
2023-02-10 21:52:46 -0500

    finished

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -24,7 +24,11 @@ function ProductsPage() {
     <>
       <h1>Products Page</h1>
       <ul>
-        {PRODUCTS.map(prod => <li key={prod.id}><Link to={`/products/${prod.id}`}>{prod.title}</Link></li>)}
+        {PRODUCTS.map((prod) => (
+          <li key={prod.id}>
+            <Link to={prod.id}>{prod.title}</Link>
+          </li>
+        ))}
       </ul>
     </>
   );
